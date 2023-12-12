ef071f7f3e8e8fafc384765aa30dcf8a219415a6
BIGGY <lorenzo.d.costa04@gmail.com>
2023-12-12 12:01:11 +0100

    Add files via upload

---
+++ backend/dipendenti/dipendenti/src/main/java/com/dipendenti/repository/RepositoryEntrate.java
@@ -0,0 +1,9 @@
+package com.dipendenti.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dipendenti.entity.Entrate;
+
+public interface RepositoryEntrate extends CrudRepository<Entrate, Long>{
+
+}
---
+++ backend/dipendenti/dipendenti/src/main/java/com/dipendenti/repository/RepositoryUscite.java
@@ -0,0 +1,9 @@
+package com.dipendenti.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.dipendenti.entity.Uscite;
+
+public interface RepositoryUscite extends CrudRepository<Uscite, Long>{
+	
+}
