1995dbbf56cb1fb947db906b29531554993aa30e
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-12 12:04:17 +0100

    Update RepositoryUscite.java

--- backend/dipendenti/dipendenti/src/main/java/com/dipendenti/repository/RepositoryUscite.java
+++ backend/dipendenti/dipendenti/src/main/java/com/dipendenti/repository/RepositoryUscite.java
@@ -1,9 +1,9 @@
 package com.dipendenti.repository;
 
 import org.springframework.data.repository.CrudRepository;
-
+import org.springframework.stereotype.Repository;
 import com.dipendenti.entity.Uscite;
-
+@Repository
 public interface RepositoryUscite extends CrudRepository<Uscite, Long>{
 	
 }
