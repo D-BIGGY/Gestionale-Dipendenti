c9e9010c1ad0de4d7b9f22fc4cca4d2777956747
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-12 12:04:45 +0100

    Update UsciteService.java

--- backend/dipendenti/dipendenti/src/main/java/com/dipendenti/service/UsciteService.java
+++ backend/dipendenti/dipendenti/src/main/java/com/dipendenti/service/UsciteService.java
@@ -2,9 +2,9 @@ package com.dipendenti.service;
 
 import java.util.List;
 import java.util.Optional;
-
+import org.springframework.stereotype.Service;
 import com.dipendenti.entity.Uscite;
-
+@Service
 public interface UsciteService {
 	
 	List<Uscite> getTutteUscite();
