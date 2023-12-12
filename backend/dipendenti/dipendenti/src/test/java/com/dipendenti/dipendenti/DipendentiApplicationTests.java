d9c95f37b27c229d78675a6f20665f025ff654ef
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-12 09:45:47 +0100

    Add files via upload

---
+++ backend/dipendenti/dipendenti/src/test/java/com/dipendenti/dipendenti/DipendentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipendenti.dipendenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DipendentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
