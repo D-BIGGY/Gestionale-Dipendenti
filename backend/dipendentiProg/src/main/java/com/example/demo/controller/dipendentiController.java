628ceb44f48b40176c99ddf7489f0fcdf248a201
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-12 16:27:29 +0100

    Update dipendentiController.java

--- backend/dipendentiProg/src/main/java/com/example/demo/controller/dipendentiController.java
+++ backend/dipendentiProg/src/main/java/com/example/demo/controller/dipendentiController.java
@@ -1,9 +1,11 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,14 +16,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Dipendente;
+import com.example.demo.entity.Entrate;
+import com.example.demo.entity.Uscite;
 import com.example.demo.service.DipendentiService;
+import com.example.demo.service.EntrateService;
+import com.example.demo.service.UsciteService;
 
 @RestController
 @RequestMapping("/gestionale/api")
+@CrossOrigin
 public class dipendentiController {
 
 	@Autowired
 	DipendentiService dipserv;
+	@Autowired
+	EntrateService entserv;
+	@Autowired
+	UsciteService uscserv;
 
 	@PostMapping("/aggiungiDipendente")
 	public void addUtente(@RequestBody Dipendente dip) {
@@ -45,4 +56,48 @@ public class dipendentiController {
 		return dipserv.modDip(id, dip);
 	}
 
+	@PostMapping("/aggEntrata")
+	public void aggEntr(Entrate entrate) {
+		entserv.save(entrate);
+	}
+
+	@GetMapping("/entrata")
+	public Optional<Entrate> getEntrate(Long id) {
+		return entserv.getEntrate(id);
+	}
+
+	@PutMapping("/modificaEntrata")
+	public void modEnt(Entrate entrate, Long id) {
+		entserv.updateEntrata(entrate, id);
+	}
+
+	@DeleteMapping("/cancEntrata")
+	public void delEnt(Long id) {
+		entserv.deleteEntrata(id);
+	}
+
+	@GetMapping("/aggUscita")
+	public void aggUsc(Uscite uscite) {
+		uscserv.saveUscita(uscite);
+	}
+
+	@GetMapping("/uscita")
+	public Optional<Uscite> getUscite(Long id) {
+		return uscserv.getUscita(id);
+	}
+
+	@PutMapping("/modificaUscita")
+	public void modUsc(Uscite uscite, Long id) {
+		uscserv.updateUscita(uscite, id);
+	}
+
+	@DeleteMapping("/cancUscita")
+	public void delUsc(Long id) {
+		uscserv.deleteUscita(id);
+	}
+
+	@GetMapping("/uscite")
+	public List<Uscite> getUscita() {
+		return uscserv.getTutteUscite();
+	}
 }
