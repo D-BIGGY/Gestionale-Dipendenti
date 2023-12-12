6be85fbe0dbba655959e5e959e063e68c89d1e38
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-12 11:37:20 +0100

    Update DipendentiService.java

--- backend/dipendenti/dipendenti/src/main/java/com/dipendenti/service/DipendentiService.java
+++ backend/dipendenti/dipendenti/src/main/java/com/dipendenti/service/DipendentiService.java
@@ -1,5 +1,26 @@
 package com.dipendenti.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.dipendenti.entity.Dipendente;
+
+import jakarta.validation.Valid;
+
+@Service
 public interface DipendentiService {
+
+	List<Dipendente> getTuttiDip();
+
+	void addDip(@Valid @RequestBody Dipendente dip);
+
+	void cancDip(@PathVariable("id") long id);
+
+	public Dipendente modDip(@PathVariable("id") long id, @Valid @RequestBody Dipendente dipendente)
+			throws MethodArgumentNotValidException;
 
 }
