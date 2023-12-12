7fba94d83811cfa1e76cf57fa90f85809857d529
ghiro122 <125474001+ghiro122@users.noreply.github.com>
2023-12-12 17:15:22 +0100

    update vari file

--- backend/dipendentiProg/src/main/java/com/example/demo/repository/DipendentiRepository.java
+++ backend/dipendentiProg/src/main/java/com/example/demo/repository/DipendentiRepository.java
@@ -1,12 +1,15 @@
 package com.example.demo.repository;
 
-
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.entity.Dipendente;
 
 @Repository
-public interface DipendentiRepository extends CrudRepository<Dipendente, Long> {
+public interface DipendentiRepository extends JpaRepository<Dipendente, Long> {
+
+	Boolean findByUsername(String username);
+
+	Boolean findByPassword(String password);
 
 }
