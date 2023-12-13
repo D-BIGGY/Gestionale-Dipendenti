4e60c09a805e1da9b67e4a2a1e140a5b493ded40
Carla Cupani <cpnclp00@gmail.com>
2023-12-13 09:45:41 +0100

    Changes to be committed:

--- backend/dipendentiProg/target/drop.sql
+++ backend/dipendentiProg/target/drop.sql
@@ -106,3 +106,9 @@
     drop table if exists entrate;
 
     drop table if exists uscite;
+
+    drop table if exists dipendente;
+
+    drop table if exists entrate;
+
+    drop table if exists uscite;
